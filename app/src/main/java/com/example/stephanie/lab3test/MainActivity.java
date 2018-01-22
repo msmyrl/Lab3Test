c607f4c4428e99b5a809dcad6047aadf88a85d97
Matthias <msmyrl@ucsd.edu>
2018-01-22 12:25:27 -0800

    The first comment

--- app/src/main/java/com/example/stephanie/lab3test/MainActivity.java
+++ app/src/main/java/com/example/stephanie/lab3test/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// Stephanie commented this time HA
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
